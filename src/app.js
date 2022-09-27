e9b2f540b2b2746752bc2cd0a3b1d8b3c0e05aaf
JackCaldwellNichols <jack.wcn@gmail.com>
2022-09-27 16:01:00 +0000

    my commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = message;
 };
+
+let pronouns = ["My", "Your", "The", "His"];
+let nouns = [" dog", " cat", " elephant", " cyclist"];
+let verb = [" peed on", " ate", " stomped on", " scratched"];
+let object = [" my homework", " my leg", " my back", " my eyes"];
+
+var message =
+  pronouns[Math.floor(Math.random() * pronouns.length)] +
+  nouns[Math.floor(Math.random() * nouns.length)] +
+  verb[Math.floor(Math.random() * verb.length)] +
+  object[Math.floor(Math.random() * object.length)];
